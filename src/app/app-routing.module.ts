b2b16c20e6e3c4d62a848d1789ac95dc0f622736
Abhijit <abhikulkarni.1988@gmail.com>
2023-05-17 14:00:24 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core"
+import { RouterModule, Routes } from "@angular/router"
+import { ContentComponent } from "./content/content.component"
+import { HeaderComponent } from "./header/header.component"
+
+const routes: Routes = [
+  {
+    path: "",
+    component: ContentComponent,
+  },
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
